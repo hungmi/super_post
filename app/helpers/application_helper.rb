12f6bce49a8d4112f04ac5ae143cb272fdb6f64b
hungmi <hungmihungmi@gmail.com>
2016-03-14 17:07:44 +0800

    版型差城市搜尋

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,15 +2,15 @@ module ApplicationHelper
   def time_since(moment)
     difference = Time.now - moment
     if difference/60/60/24/7 >= 1
-      return "#{(difference/60/60/24/7).round} weeks ago"
+      return "#{(difference/60/60/24/7).round} weeks"
     elsif difference/60/60/24 >= 1
-      return "#{(difference/60/60/24).round} days ago"
+      return "#{(difference/60/60/24).round} days"
     elsif difference/60/60 >= 1
-      return "#{(difference/60/60).round} hours ago"
+      return "#{(difference/60/60).round} hours"
     elsif difference/60 >= 1
-      return "#{(difference/60).round} mins ago"
+      return "#{(difference/60).round} mins"
     else
-      return "#{(difference).round} secs ago"
+      return "#{(difference).round} secs"
     end
   end
 end
